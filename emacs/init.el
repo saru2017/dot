541cf7729bec7d3dd7f0248cbf62be503a9598b1
saru <saru@ist.osaka-u.ac.jp>
2021-01-29 20:31:34 +0900

    updated emacs

--- emacs/init.el
+++ emacs/init.el
@@ -57,9 +57,19 @@
 ;;; modelineの色
 (set-face-background 'mode-line "Cyan") 
 
-;;; package
+
+;;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; ;;;
+;;; @ package manager                                               ;;;
+;;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; ;;;
+
+(require 'package)
+(add-to-list 'package-archives
+             '("melpa" . "http://melpa.milkbox.net/packages/") t)
+(add-to-list 'package-archives
+             '("marmalade" . "http://marmalade-repo.org/packages/"))
 (add-to-list 'package-archives
              '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+(package-initialize)
 
 ;;
 ;; YaTeX
@@ -99,3 +109,199 @@
 (set-charset-priority 'ascii 'japanese-jisx0208 'latin-jisx0201
                       'katakana-jisx0201 'iso-8859-1 'cp1252 'unicode)
 (set-coding-system-priority 'utf-8 'euc-jp 'iso-2022-jp 'cp932)
+
+
+;;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; ;;;
+;;; @ screen - buffer                                               ;;;
+;;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; ;;;
+
+;; バッファ画面外文字の切り詰め表示（有効：t、無効：nil）
+(setq truncate-lines nil)
+
+;; ウィンドウ縦分割時のバッファ画面外文字の切り詰め表示（有効：t、無効：nil）
+(setq truncate-partial-width-windows t)
+
+;; 同一バッファ名にディレクトリ付与
+(require 'uniquify)
+(setq uniquify-buffer-name-style 'forward)
+(setq uniquify-buffer-name-style 'post-forward-angle-brackets)
+(setq uniquify-ignore-buffers-re "*[^*]+*")
+
+;;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; ;;;
+;;; @ screen - linum                                                ;;;
+;;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; ;;;
+
+(require 'linum)
+
+;; 行移動を契機に描画
+(defvar linum-line-number 0)
+(declare-function linum-update-current "linum" ())
+(defadvice linum-update-current
+    (around linum-update-current-around activate compile)
+  (unless (= linum-line-number (line-number-at-pos))
+    (setq linum-line-number (line-number-at-pos))
+    ad-do-it
+    ))
+
+;; バッファ中の行番号表示の遅延設定
+(defvar linum-delay nil)
+(setq linum-delay t)
+(defadvice linum-schedule (around linum-schedule-around () activate)
+  (run-with-idle-timer 1.0 nil #'linum-update-current))
+
+;; 行番号の書式
+(defvar linum-format nil)
+(setq linum-format "%5d ")
+
+;; バッファ中の行番号表示（有効：t、無効：nil）
+(global-linum-mode t)
+
+(custom-set-faces
+ '(linum ((t (:inherit (shadow default) :background "Gray23")))))
+
+;;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; ;;;
+;;; @ search - isearch                                              ;;;
+;;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; ;;;
+
+;; 大文字・小文字を区別しないでサーチ（有効：t、無効：nil）
+;;(setq-default case-fold-search nil)
+(setq-default case-fold-search nil)
+
+;; C-dで検索文字列を一文字削除
+(define-key isearch-mode-map (kbd "C-d") 'isearch-delete-char)
+
+;; C-yで検索文字列にヤンク貼り付け
+(define-key isearch-mode-map (kbd "C-y") 'isearch-yank-kill)
+
+;; C-eで検索文字列を編集
+(define-key isearch-mode-map (kbd "C-e") 'isearch-edit-string)
+
+;; Tabで検索文字列を補完
+(define-key isearch-mode-map (kbd "TAB") 'isearch-yank-word)
+
+;; C-gで検索を終了
+(define-key isearch-mode-map (kbd "C-g")
+  '(lambda() (interactive) (isearch-done)))
+
+;;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; ;;;
+;;; @ file - backup                                                 ;;;
+;;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; ;;;
+
+;; ファイルオープン時のバックアップ（~）（有効：t、無効：nil）
+(setq make-backup-files   t)  ;; 自動バックアップの実行有無
+(setq version-control     t)  ;; バックアップファイルへの番号付与
+(setq kept-new-versions   3)  ;; 最新バックアップファイルの保持数
+(setq kept-old-versions   0)  ;; 最古バックアップファイルの保持数
+(setq delete-old-versions t)  ;; バックアップファイル削除の実行有無
+
+;; ファイルオープン時のバックアップ（~）の格納ディレクトリ
+(setq backup-directory-alist
+      (cons (cons "\\.*$" (expand-file-name "/tmp/emacsbk"))
+            backup-directory-alist))
+
+;; 編集中ファイルの自動バックアップ（有効：t、無効：nil）
+(setq backup-inhibited nil)
+
+;; 終了時に自動バックアップファイルを削除（有効：t、無効：nil）
+(setq delete-auto-save-files nil)
+
+;; 編集中ファイルのバックアップ（有効：t、無効：nil）
+(setq auto-save-list-file-name nil)
+(setq auto-save-list-file-prefix nil)
+
+;; 編集中ファイルのバックアップ間隔（秒）
+(setq auto-save-timeout 3)
+
+;; 編集中ファイルのバックアップ間隔（打鍵）
+(setq auto-save-interval 100)
+
+;; 編集中ファイル（##）の格納ディレクトリ
+(setq auto-save-file-name-transforms
+      `((".*" ,(expand-file-name "/tmp/emacsbk") t)))
+
+
+;;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; ;;;
+;;; @ file - lockfile                                               ;;;
+;;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; ;;;
+
+;; ロックファイルを生成（有効：t、無効：nil）
+(setq create-lockfiles nil)
+
+
+;;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; ;;;
+;;; @ scroll                                                        ;;;
+;;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; ;;;
+
+;; スクロール時のカーソル位置を維持（有効：t、無効：nil）
+(setq scroll-preserve-screen-position t)
+
+;; スクロール開始の残り行数
+(setq scroll-margin 0)
+
+;; スクロール時の行数
+(setq scroll-conservatively 10000)
+
+;; スクロール時の行数（scroll-marginに影響せず）
+(setq scroll-step 0)
+
+;; 画面スクロール時の重複表示する行数
+(setq next-screen-context-lines 1)
+
+;; キー入力中の画面更新を抑止（有効：t、無効：nil）
+(setq redisplay-dont-pause t)
+
+;; recenter-top-bottomのポジション
+(setq recenter-positions '(middle top bottom))
+
+;; 横スクロール開始の残り列数
+(setq hscroll-margin 1)
+
+;; 横スクロール時の列数
+(setq hscroll-step 1)
+
+;; スクロールダウン
+(global-set-key (kbd "C-z") 'scroll-down)
+
+;; バッファの最後までスクロールダウン
+(defadvice scroll-down (around scroll-down activate compile)
+  (interactive)
+  (let (
+        (bgn-num (+ 1 (count-lines (point-min) (point))))
+        )
+    (if (< bgn-num (window-height))
+        (goto-char (point-min))
+      ad-do-it) ))
+
+;; バッファの先頭までスクロールアップ
+(defadvice scroll-up (around scroll-up activate compile)
+  (interactive)
+  (let (
+        (bgn-num (+ 1 (count-lines (point-min) (point))))
+        (end-num nil)
+        )
+    (save-excursion
+      (goto-char (point-max))
+      (setq end-num (+ 1 (count-lines (point-min) (point))))
+      )
+    (if (< (- (- end-num bgn-num) (window-height)) 0)
+        (goto-char (point-max))
+      ad-do-it) ))
+
+;; saru
+
+(setq inhibit-startup-message t)
+;; global set key
+(global-set-key "\C-l" 'goto-line)
+
+(add-hook 'c-mode-common-hook
+          '(lambda ()
+              (c-set-style "k&r")
+              (setq c-basic-offset 2 indent-tabs-mode nil)
+                          (c-toggle-auto-hungry-state 1)))
+
+(autoload 'dockerfile-mode "dockerfile-mode" nil t)
+(add-to-list 'auto-mode-alist '("Dockerfile\\'" . dockerfile-mode))
+(require 'php-mode)
+(show-paren-mode t)
+(setq ring-bell-function 'ignore)
+(menu-bar-mode 0) 
