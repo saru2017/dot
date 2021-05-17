7511027b1056ce45219cdff53235141b993b16f3
saru <saru@ist.osaka-u.ac.jp>
2021-05-17 18:19:42 +0900

    minor update.

--- emacs/init.el
+++ emacs/init.el
@@ -1,2 +1,41 @@
+;;; packet自動インストール用
+(require 'package)
+(setq package-archives
+      '(("gnu" . "https://elpa.gnu.org/packages/")
+	("melpa" . "https://melpa.org/packages/")
+	("org" . "http://orgmode.org/elpa/")))
+(package-initialize)
+
+(defun require-package (package &optional min-version no-refresh)
+  "Install given PACKAGE, optionally requiring MIN-VERSION.
+If NO-REFRESH is non-nil, the available package lists will not be
+re-downloaded in order to locate PACKAGE."
+  (if (package-installed-p package min-version)
+      t
+    (if (or (assoc package package-archive-contents) no-refresh)
+	(if (boundp 'package-selected-packages)
+	    ;; Record this as a package the user installed explicitly
+	    (package-install package nil)
+	  (package-install package))
+      (progn
+        (package-refresh-contents)
+        (require-package package min-version t)))))
+
+(defun maybe-require-package (package &optional min-version no-refresh)
+  "Try to install PACKAGE, and return non-nil if successful.
+In the event of failure, return nil and print a warning message.
+Optionally require MIN-VERSION.  If NO-REFRESH is non-nil, the
+available package lists will not be re-downloaded in order to
+locate PACKAGE."
+  (condition-case err
+      (require-package package min-version no-refresh)
+    (error
+     (message "Couldn't install optional package `%s': %S" package err)
+     nil)))
+
+
+;;; 環境毎に変更
 (when (equal window-system 'w32)
   (load-file "~/dot/emacs/init-win.el"))
+(when (equal window-system 'nil)
+  (load-file "~/dot/emacs/init-linux.el"))
