643dab9bd7cdb51815ddde2ff4052ecdd0f0e8f7
saru <saru@ist.osaka-u.ac.jp>
2022-08-12 16:47:14 -0700

    update for ubuntu 20.04 desktop@

--- tmux/verify_environment.sh
+++ tmux/verify_environment.sh
@@ -4,6 +4,9 @@ verify_environment(){
     if [ -n "$WSLENV" ]; then
 	tmux source ~/dot/tmux/tmux_win.conf
 	exit
+    elif [ -n "$GNOME_DESKTOP_SESSION_ID" ]; then
+	tmux source ~/dot/tmux/tmux_desktop.conf	
+	exit
     elif [ `curl -m 5 -qf http://169.254.169.254/latest/meta-data/instance-id 2>/dev/null | grep "i-" | wc -l` -gt 0 ]; then
 	tmux source ~/dot/tmux/tmux_aws.conf	
 	exit
